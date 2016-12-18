d9bcdab5e7e64fc0b0c8d258ca2a3c1bf9e8ecf6
Henry Middleton <h78900@gmail.com>
2016-12-18 16:47:48 -0500

    first iteration complete

---
+++ squares.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+  makeGrid(50, 50);
+
+  $('#make').click(function() {
+    var s = prompt("How many squares across?");
+    console.log('hi');
+    makeGrid(s, s);
+  });
+
+});
+
+function makeGrid(h, w) {
+  $('.row').remove();
+  var height = h;
+  var width = w;
+  var square_height = 960/height;
+  var square_width = 960/width;
+  var row = '<div class="row"></div>';
+  var square = '<div class="square"></div>';
+  var drawing = true;
+
+  divs = [];
+  for (var i = 0; i < height; i++) {
+    $('#wrapper').append(row);
+  }
+  for (var j = 0; j < width; j++) {
+    $('.row').append(square);
+  }
+
+  $('.square').css('width', square_width);
+  $('.square').css('height', square_height);
+
+  $('#clear').click(function() {
+    $('.square').css('background-color', 'white');
+    drawing = true;
+  });
+
+  $('.square').mouseenter(function() {
+    console.log('yes');
+    if (drawing==true) {
+      $(this).css('background-color', 'black');
+      console.log('yes');
+    }
+  });
+
+  $(document).click(function() {
+    drawing = !drawing;
+  });
+
+}
